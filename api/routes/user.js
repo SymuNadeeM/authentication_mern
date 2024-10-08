a46821e6e00830cd6e4a74baff934d1c7dd7d796
SymuNadeem <msymunnadeem@gmail.com>
2024-10-08 17:12:46 +0600

    bearer added in token

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const authenticate = require("../middleware/authenticate")
 
 const userController = require("../controllers/user");
 
 router.post("/login", userController.loginController);
 router.post("/register", userController.registerController);
-router.get("/", userController.getAllUsers);
+router.get("/",authenticate, userController.getAllUsers);
 
 module.exports = router;
